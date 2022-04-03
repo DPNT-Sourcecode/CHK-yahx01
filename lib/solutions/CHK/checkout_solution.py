4a9f132ecb83b6bf595bdb7d309c6a7531cd7ef9
sbx_user1051 <sbx_user1051@169.254.125.13>
2022-04-03 19:08:56 +0000

    Sun Apr 03 19:08:56 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,33 @@
 from collections import Counter
 
-prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40,
+    "F": 10,
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 80,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 30,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 90,
+    "Y": 10,
+    "Z": 50,
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -34,4 +61,5 @@ def special_offers(counter):
 
 def normal_prices(counter):
     yield from (prices[sku] * count for sku, count in counter.most_common())
+
 
