7e82abb644171a08b830aea167d4ff413ee08749
sbx_user1051 <sbx_user1051@169.254.125.13>
2022-04-03 18:38:11 +0000

    Sun Apr 03 18:38:11 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,19 @@
+from collections import Counter
+
+prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if not skus.isalpha():
+        return -1
+
+    frequencies = Counter()
+    for letter in skus.lower():
+        frequencies[letter] += 1
+
+
+def is_latin_alphabet(letter):
+    return
+
 
