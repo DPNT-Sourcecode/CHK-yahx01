42580da4eeb4c5d1eea0de1920ed757cd46eb1b6
sbx_user1051 <sbx_user1051@169.254.125.13>
2022-04-03 18:25:57 +0000

    Sun Apr 03 18:25:57 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,11 +1,12 @@
-from solutions.SUM import sum_solution
+from lib.solutions.SUM import sum_solution
 
 import unittest
 
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
-        for x in range(0, 10):
-            for y in range(0, 10):
-                self.assertEquals(sum_solution.compute(x, y), x + y)
+        for x in range(0, 100):
+            for y in range(0, 100):
+                self.assertEqual(sum_solution.compute(x, y), x + y)
+
 
