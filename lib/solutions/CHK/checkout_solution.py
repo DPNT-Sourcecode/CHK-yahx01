1b891f644583195db4cf2cb340c59701d4c582a9
sbx_user1051 <sbx_user1051@169.254.125.13>
2022-04-03 18:58:53 +0000

    Sun Apr 03 18:58:53 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,28 +8,30 @@ def checkout(all_skus):
     if any(c not in prices.keys() for c in all_skus):
         return -1
 
-    frequencies = Counter()
+    counter = Counter()
     for sku in all_skus:
-        frequencies[sku] += 1
+        counter[sku] += 1
 
-    return sum(price(sku, count) for sku, count in frequencies.most_common())
-
-
-def price(sku, count):
-    return special_offers(counter) + normal_prices(counter)
+    return sum(special_offers(counter)) + sum(normal_prices(counter))
 
 
 def special_offers(counter):
     if counter["A"]:
         yield (counter["A"] // 3) * 130
         counter["A"] %= 3
+
+    # Get a free B with every 2 E
+    counter["B"] = max(0, counter["B"] - counter["E"] // 2)
+
     if counter["B"]:
-        yield (counter["B"] // 2) * 130
+        yield (counter["B"] // 2) * 45
         counter["B"] %= 2
 
 
 def normal_prices(counter):
-    return sum(prices[sku] * count for sku, count in frequencies.most_common())
+    print(counter.most_common())
+    yield from (prices[sku] * count for sku, count in counter.most_common())
+
 
 
 
